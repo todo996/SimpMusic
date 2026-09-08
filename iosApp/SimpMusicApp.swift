500644ee87584c692ad017b21d11db75b499a5fb
todo996 <kuquan55555@gmail.com>
2026-09-09 01:52:17 +0700

    feat(ios): add SwiftUI host app

---
+++ iosApp/SimpMusicApp.swift
@@ -0,0 +1,20 @@
+import SwiftUI
+import ComposeApp
+
+struct ComposeViewController: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> UIViewController {
+        MainViewControllerKt.MainViewController()
+    }
+
+    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {}
+}
+
+@main
+struct SimpMusicIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ComposeViewController()
+                .ignoresSafeArea(.keyboard)
+        }
+    }
+}
