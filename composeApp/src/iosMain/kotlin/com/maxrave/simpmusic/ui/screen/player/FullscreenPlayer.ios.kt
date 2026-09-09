727ee96ec8474a727f84c12720e8b37a559fef63
todo996 <kuquan55555@gmail.com>
2026-09-09 08:45:53 +0700

    feat(ios): add fullscreen lifecycle actual

---
+++ composeApp/src/iosMain/kotlin/com/maxrave/simpmusic/ui/screen/player/FullscreenPlayer.ios.kt
@@ -0,0 +1,15 @@
+package com.maxrave.simpmusic.ui.screen.player
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+
+@Composable
+actual fun FullScreenRotationImmersive(
+    onLaunch: () -> Unit,
+    onDispose: () -> Unit,
+) {
+    DisposableEffect(Unit) {
+        onLaunch()
+        onDispose { onDispose() }
+    }
+}
